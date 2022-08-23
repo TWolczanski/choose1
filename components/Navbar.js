8d39c0f274d2d5271ba4217eec16a6c50ff32126
Tomek <tomasz.wolczanski@proton.me>
2022-08-24 00:58:31 +0200

    Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import Logo from "./Logo";
+import Nav from "./Nav";
+import Button from "./Button";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className={styles.navbar}>
+      <div className={`container ${styles.container}`}>
+        <Link href="/"><a><Logo variant="dark" /></a></Link>
+        <Nav />
+        <div className={styles.buttons}>
+          <Button variant="light" text="Sign in"></Button>
+          <Button variant="primary" text="Sign up"></Button>
+        </div>
+      </div>
+    </header>
   );
 }
