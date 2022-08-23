d4d81fa74c827fc1e0109baa3950350532a1af49
Tomek <tomasz.wolczanski@proton.me>
2022-08-23 15:14:23 +0200

    Button component

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Button.module.css";
+
+export default function Button({variant, text, onClick}) {
+	return (
+		<button className={`${styles.btn} ${styles[variant]}`} onClick={onClick}>{text}</button>
+	);
+}
+
+Button.defaultProps = {
+	variant: "light"
+}
