9efd40f5e727ab316125096043b4fdb67c734c94
Tomek <tomasz.wolczanski@proton.me>
2022-09-07 02:07:48 +0200

    Hero component

---
+++ components/Hero.js
@@ -0,0 +1,32 @@
+import styles from "../styles/Hero.module.css";
+import Button from "./Button";
+import Link from "next/link";
+import CarouselPost from "./CarouselPost";
+
+export default function Hero({postData}) {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.main}>
+        <h1>Help and get help with making design decisions.</h1>
+        <p>
+          Having trouble selecting the best logo concept or the proper color
+          scheme in your UI project? Or maybe you can’t decide on the camera
+          angle in your 3D render? Post your work and see what others have to
+          say about it.
+        </p>
+        <Link href="/about">
+          <a>
+            <Button variant="secondary" text="Learn more" />
+          </a>
+        </Link>
+      </div>
+      <CarouselPost
+        images={postData.images}
+        results={postData.results}
+        animation
+        showPercentage
+        className={styles.post}
+      />
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-export default function home() {
+import Hero from "../components/Hero";
+import {server} from "../config";
+
+export default function home({heroPostData}) {
   return (
-    <div></div>
+    <Hero postData={heroPostData} />
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${server}/data/posts.json`);
+  const posts = await res.json();
+  const heroPostData = posts[Math.floor(Math.random() * posts.length)]; 
+  return {
+    props: {
+      heroPostData
+    }
+  };
 }
