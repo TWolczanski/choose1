8eedb284c534421cf28f742f6a0869af1a7abb42
Tomek <tomasz.wolczanski@proton.me>
2022-09-08 21:58:25 +0200

    CarouselPost - added the possibility to choose a design

--- components/CarouselPost.js
+++ components/CarouselPost.js
@@ -15,28 +15,44 @@ export default function CarouselPost({
   roundedCorners,
   className,
 }) {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(1);
+  const [chosen, setChosen] = useState(choice);
 
   return (
     <div className={`${postStyles.post} ${className ? className : ""}`}>
       <div
         className={`
           ${styles.imageWrapper}
-          ${clickable ? styles.clickable : ""}
+          ${clickable && !chosen ? styles.clickable : ""}
           ${roundedCorners ? styles.rounded : ""}
         `}
+        onClick={() => {
+          if (!chosen) {
+            setChosen(active);
+          }
+        }}
       >
         <Image
-          src={images[active]}
+          src={images[active - 1]}
           layout="fill"
           objectFit="contain"
           priority={true}
         />
+        {clickable && (
+          <div
+            className={`${styles.overlay} ${
+              active === chosen ? styles.secondary : styles.primary
+            }`}
+          ></div>
+        )}
       </div>
       <ResultsBar
-        results={(choice || !clickable) && results}
-        onClicks={images.map((img, i) => () => setActive(i))}
-        highlighted={active + 1}
+        results={
+          (chosen || !clickable) &&
+          results.map((r, i) => (i + 1 === chosen ? r + 1 : r))
+        }
+        onClicks={images.map((img, i) => () => setActive(i + 1))}
+        highlighted={active}
         showPercentage={showPercentage}
       />
       <span className={postStyles.title}>{title}</span>
