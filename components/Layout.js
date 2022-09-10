657054cf3d496a19d83c640cc65b9634423e00ec
Tomek <tomasz.wolczanski@proton.me>
2022-09-10 21:26:44 +0200

    container redefinition

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,9 @@ import Navbar from "./Navbar";
 
 export default function Layout({children}) {
   return (
-    <>
+    <div className="container">
       <Navbar />
       {children}
-    </>
+    </div>
   );
 }
