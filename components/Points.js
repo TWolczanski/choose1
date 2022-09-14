f64c37c37fdaca8ef42b32c20a8ed1dfd8073160
Tomek <tomasz.wolczanski@proton.me>
2022-09-14 21:01:41 +0200

    Points - added the className prop

--- components/Points.js
+++ components/Points.js
@@ -1,9 +1,9 @@
 import styles from "../styles/Points.module.css";
 import Image from "next/image";
 
-export default function Points({amount}) {
+export default function Points({amount, className}) {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${className}`}>
       <div className={styles.iconWrapper}>
         <Image src="/img/point-icon.svg" layout="fill" objectFit="contain" />
       </div>
