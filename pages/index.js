f486c641ad8c4c9a04a304e814c75f4f17583034
Tomek <tomasz.wolczanski@proton.me>
2022-09-15 18:01:41 +0200

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,40 @@
 import Hero from "../components/Hero";
+import Posts from "../components/Posts";
+import Button from "../components/Button";
+import UserRanking from "../components/UserRanking";
+import Link from "next/link";
 import {server} from "../config";
+import styles from "../styles/home.module.css";
 
 export default function home({heroPostData}) {
   return (
-    <Hero postData={heroPostData} />
+    <>
+      <Hero postData={heroPostData} />
+      <h1 className={styles.header}>Recent posts</h1>
+      <Posts />
+      <Link href="/posts">
+        <a className={styles.viewMore}>
+          <Button variant="secondary" text="View more" />
+        </a>
+      </Link>
+      <h1 className={styles.header}>Top users</h1>
+      <UserRanking top={3} />
+      <Link href="/users">
+        <a className={styles.viewMore}>
+          <Button variant="secondary" text="View more" />
+        </a>
+      </Link>
+    </>
   );
 }
 
 export async function getServerSideProps() {
   const res = await fetch(`${server}/data/posts.json`);
   const posts = await res.json();
-  const heroPostData = posts[Math.floor(Math.random() * posts.length)]; 
+  const heroPostData = posts[Math.floor(Math.random() * posts.length)];
   return {
     props: {
-      heroPostData
-    }
+      heroPostData,
+    },
   };
 }
