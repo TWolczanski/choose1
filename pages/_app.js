8bb5b86d364c1c6c4fc9c2be42369dd809d2472e
Tomek <tomasz.wolczanski@proton.me>
2023-02-24 21:38:55 +0100

    wrapped the app in UserProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import {ModalProvider} from "../context/ModalContext";
+import {UserProvider} from "../context/UserContext";
 
 function MyApp({Component, pageProps}) {
   return (
-    <ModalProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ModalProvider>
+    <UserProvider>
+      <ModalProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ModalProvider>
+    </UserProvider>
   );
 }
 
