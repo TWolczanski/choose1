09ab7bdeb39791d6c64c9b2ebcd45ed0044ae38c
Tomek <tomasz.wolczanski@proton.me>
2023-02-24 22:10:59 +0100

    PostsPage - handling signed in/out user

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,14 @@
 import Posts from "../../components/Posts";
 import NewPostButton from "../../components/NewPostButton";
 import styles from "../../styles/PostsPage.module.css";
+import {useUser} from "../../context/UserContext";
 
 export default function PostsPage() {
+  const {user} = useUser();
   return (
     <>
       <h1 className="header">Posts</h1>
-      <NewPostButton className={styles.newPostBtn} />
+      {user && <NewPostButton className={styles.newPostBtn} />}
       <Posts />
     </>
   );
