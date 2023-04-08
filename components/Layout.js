3efe60ffad002b453df763242945e2e4a3885528
Tomek <tomasz.wolczanski@proton.me>
2023-04-08 13:47:06 +0200

    pre-deploy changes

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,23 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 import {useModal} from "../context/ModalContext";
+import {useEffect} from "react";
+import WebsiteInfo from "./WebsiteInfo";
+import Head from "next/head";
 
 export default function Layout({children}) {
-  const {open} = useModal();
+  const {open, setContent} = useModal();
+
+  useEffect(() => {
+    setContent(<WebsiteInfo />);
+  }, []);
+
   return (
     <div className={`${styles.container} ${open ? styles.modalOpen : ""}`}>
+      <Head>
+        <title>choose1</title>
+        <link rel="icon" href="/img/favicon.png" />
+      </Head>
       <Navbar />
       <div className={styles.content}>{children}</div>
       <Footer />
---
+++ components/WebsiteInfo.js
@@ -0,0 +1,13 @@
+import styles from "../styles/WebsiteInfo.module.css";
+import Paragraph from "./Paragraph";
+
+export default function WebsiteInfo() {
+  const info =
+    "The website has been prepopulated with data taken from dribbble.com. This is solely for presentational purposes; all rights to designs posted here belong to their respective authors. The project is non-commercial and is hosted on GitHub (TWolczanski/choose1).\n\nThe backend hasn't been implemented yet. To view the page as a logged in user, try signing in with a username of 'admin' and a password of 'pass'.";
+  return (
+    <div className={styles.websiteInfo}>
+      <h1>Info</h1>
+      <Paragraph text={info} />
+    </div>
+  );
+}
